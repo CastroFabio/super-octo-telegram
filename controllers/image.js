e7e36ce9bce3c30a3c1e118328717689ff576f39
CastroFabio <fmpc95@gmail.com>
2021-08-15 15:55:34 -0300

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: '93cf365fc3094d8ebd02d806579dd877'
+  })
+
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(
+            Clarifai.CELEBRITY_MODEL,
+            req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then( entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
